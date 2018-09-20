d925eccbe6260c10b12f25da2fa8e7c1f73b2348
Florian Keller <github@floriankeller.de>
2018-09-20 19:29:13 +0200

    refactor: Use TypeScript in tests

--- spec/MyTimezone.spec.ts
+++ spec/MyTimezone.spec.ts
@@ -1,22 +1,20 @@
-//@ts-check
-
-const { MyTimezone } = require('../');
-const nock = require('nock');
+import {MyTimezone} from '../src';
+import * as nock from 'nock';
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10 * 1000; // 10 seconds
 
 describe('MyTimezone', () => {
-  let tz;
+  let tz: MyTimezone;
 
   beforeEach(() => {
     tz = new MyTimezone({
       offline: true
     });
 
-    let formatted_address;
+    let formatted_address: string;
     nock('https://maps.googleapis.com')
       .get('/maps/api/geocode/json')
-      .query(obj => {
+      .query((obj: {address: string}) => {
         formatted_address = obj.address;
         return true;
       })
@@ -48,10 +46,10 @@ describe('MyTimezone', () => {
   });
 
   it('returns the correct time for a location', async () => {
-    const berlinTime = await tz.getTimeByLocation(52.51848, 13.40803);
+    const berlinTime = await tz.getTimeByLocation(13.40803);
     //console.log('Timezone at 52.51848, 13.40803:', berlinTime.toString());
 
-    const frankfurtTime = await tz.getTimeByLocation(50.11796, 8.67931);
+    const frankfurtTime = await tz.getTimeByLocation(8.67931);
     //console.log('Timezone at 50.11796, 8.67931:', frankfurtTime.toString());
 
     expect(frankfurtTime.isBefore(berlinTime)).toBe(true);
@@ -59,15 +57,17 @@ describe('MyTimezone', () => {
 
   it('returns the time for an address', async () => {
     const dataBerlin = await tz.getTimeByAddress('Berlin, Germany');
+    expect(dataBerlin).toBeDefined();
     //console.log('Timezone Berlin:', dataBerlin.toString());
 
     const dataMinsk = await tz.getTimeByAddress('Minsk, Belarus');
+    expect(dataMinsk).toBeDefined();
     //console.log('Timezone Minsk:', dataMinsk.toString());
   });
 
   it('calculates the distance between two locations', () => {
-    const result1 = tz.calculateDistance(13.3497483, 52.5303654);
-    const result2 = tz.calculateDistance(52.5303654, 13.3497483);
+    const result1 = tz['calculateDistance'](13.3497483, 52.5303654);
+    const result2 = tz['calculateDistance'](52.5303654, 13.3497483);
     expect(result1).toEqual(39.1806171);
     expect(result2).toEqual(39.1806171);
   });
