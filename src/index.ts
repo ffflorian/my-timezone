cbedec3ded3e961532704c86fa7b57c6c32e7629
Florian Keller <ffflorian@users.noreply.github.com>
2019-03-13 14:48:10 +0100

    chore: Add prettierrc file from jszip-cli (#167)

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import axios from 'axios';
 import * as moment from 'moment';
 import NTPClient from 'ntpclient';
-import {
-  Coordinates,
-  GoogleMapsResult,
-  Location,
-  MyTimezoneConfig
-} from './Interfaces';
+import {Coordinates, GoogleMapsResult, Location, MyTimezoneConfig} from './Interfaces';
 
 const defaultConfig: Required<MyTimezoneConfig> = {
   ntpServer: 'pool.ntp.org',
-  offline: false
+  offline: false,
 };
 
 export class MyTimezone {
@@ -20,19 +15,15 @@ export class MyTimezone {
   constructor(config?: MyTimezoneConfig) {
     this.config = {
       ...defaultConfig,
-      ...config
+      ...config,
     };
     this.ntpClient = new NTPClient(this.config.ntpServer);
   }
 
-  public async getLocationByName(
-    address: string,
-    radius = ''
-  ): Promise<Location> {
+  public async getLocationByName(address: string, radius = ''): Promise<Location> {
     const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json';
     address = encodeURIComponent(address);
-    const completeURL = `${baseURL}?address=${address}${radius &&
-      `&radius=${radius}`}`;
+    const completeURL = `${baseURL}?address=${address}${radius && `&radius=${radius}`}`;
 
     let data: GoogleMapsResult | undefined;
 
@@ -50,20 +41,20 @@ export class MyTimezone {
       throw new Error('Unknown Google Maps API Error.');
     }
 
-    const { results = [] } = data;
+    const {results = []} = data;
 
     if (!results.length) {
       throw new Error('No place found.');
     }
 
     const {
-      geometry: { location },
-      formatted_address
+      geometry: {location},
+      formatted_address,
     } = results[0];
 
     return {
       formattedAddress: formatted_address,
-      longitude: location.lng
+      longitude: location.lng,
     };
   }
 
@@ -73,7 +64,7 @@ export class MyTimezone {
     if (data && data.length > 0) {
       try {
         const longitude = parseFloat(data[1]);
-        return { longitude };
+        return {longitude};
       } catch (error) {
         throw new Error(`Invalid coordinates: "${coordinates}"`);
       }
@@ -103,13 +94,11 @@ export class MyTimezone {
     const distance = this.calculateDistance(0, longitude);
     const distanceSeconds = distance / 0.004167;
 
-    return longitude < 0
-      ? momentDate.subtract(distanceSeconds, 'seconds')
-      : momentDate.add(distanceSeconds, 'seconds');
+    return longitude < 0 ? momentDate.subtract(distanceSeconds, 'seconds') : momentDate.add(distanceSeconds, 'seconds');
   }
 
   public async getTimeByAddress(address: string): Promise<moment.Moment> {
-    const { longitude } = await this.getLocationByName(address);
+    const {longitude} = await this.getLocationByName(address);
     return this.getTimeByLocation(longitude);
   }
 }
